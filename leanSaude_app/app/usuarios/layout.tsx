5a1dd94a0c1b76e43684fd765ba13ecbe8ec8090
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-04-11 18:59:14 -0300

    :sparkles: feat: header

--- leanSaude_app/app/usuarios/layout.tsx
+++ leanSaude_app/app/usuarios/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/header";
 import type React from "react";
 
 export default function UsuariosLayout({
@@ -7,6 +8,7 @@ export default function UsuariosLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex-1 container mx-auto py-6 px-4">{children}</main>
     </div>
   );
---
+++ leanSaude_app/components/header.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut } from "lucide-react";
+
+export function Header() {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: "Clientes", href: "/usuarios" },
+    { name: "Endereços", href: "/login" },
+    { name: "Entregas", href: "/login" },
+  ];
+
+  return (
+    <header className="border-b">
+      <div className="container flex h-16 items-center px-4">
+        <div className="mr-8 text-lg font-medium text-gray-500">LOGO</div>
+        <nav className="flex items-center space-x-6 text-sm font-medium">
+          {navItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={cn(
+                "transition-colors hover:text-primary",
+                pathname.startsWith(item.href)
+                  ? "text-primary border-b-2 border-primary "
+                  : "text-muted-foreground"
+              )}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+        <div className="ml-auto flex items-center space-x-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                <Avatar className="h-8 w-8 bg-primary">
+                  <AvatarFallback>{"U"}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Sair</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
